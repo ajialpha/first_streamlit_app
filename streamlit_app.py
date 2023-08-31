5c267c2be37f2c7847fe7acade965c9555c2a60f
ajialpha <143673080+ajialpha@users.noreply.github.com>
2023-08-31 13:01:35 +1000

    Update streamlit_app.py
    New header - own smoothie

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,4 @@ streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Bolied Free-Range Egg')
 streamlit.text('🥑🍞 Avacado Toast')
 
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
